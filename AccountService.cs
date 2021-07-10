7734f94dd16624175bd06e6cc100b83e592d9ab4
Ghidyon <gideonakunana@gmail.com>
2021-07-10 13:03:55 +0200

    fixed login validation and prompt

--- AccountService.cs
+++ AccountService.cs
@@ -24,39 +24,48 @@ namespace OOP
 
             Console.WriteLine($"Congratulations {_user.FullName}! Your registration was successful!");
 
-            Console.WriteLine("Press 1 to login:\n");
-            var input = Console.ReadLine();
-
-            if(input == "1")
+            bool isLoggedIn = false;
+            while (!isLoggedIn)
             {
-                Console.WriteLine("Enter your Email and password seperated with space");
-                var credentials = Console.ReadLine();
-                if ( !string.IsNullOrWhiteSpace(credentials) && credentials.Split().Length > 1 && credentials.Split().Length < 3 )
-                {
-                    var email = credentials.Split()[0].Trim().ToLower();
-                    var passWord = credentials.Split()[1];
-                    Login(email, passWord);
-                }
-                else
-                {
-                    while (string.IsNullOrWhiteSpace(credentials) || !(credentials.Split().Length > 1 && credentials.Split().Length < 3))
-                    {
-                        Console.WriteLine("The field is required!");
-                        Console.WriteLine("Enter your Email and password seperated with space");
-                        credentials = Console.ReadLine();
-                    }
-                    var email = credentials.Split()[0].Trim().ToLower();
-                    var passWord = credentials.Split()[1];
-                    Login(email, passWord);
-                }
-            } 
-            else
-            {
-                Console.WriteLine("Invalid Input");
+                isLoggedIn = PromptLogin();
             }
         }
 
-        public static void Login(string email, string password)
+        public static bool PromptLogin()
+        {
+            Console.WriteLine("Press 1 to login:\n");
+            var input = Console.ReadLine().Trim();
+
+            while (input != "1")
+            {
+                Console.WriteLine("Invalid Input");
+                Console.WriteLine("Press 1 to login:\n");
+                input = Console.ReadLine().Trim();
+            }
+
+            Console.WriteLine("Enter your Email and Password seperated by a space");
+            var credentials = Console.ReadLine();
+            if (!string.IsNullOrWhiteSpace(credentials) && credentials.Split().Length > 1 && credentials.Split().Length < 3)
+            {
+                var email = credentials.Split()[0].Trim().ToLower();
+                var passWord = credentials.Split()[1];
+                return ValidateLogin(email, passWord);
+            }
+            else
+            {
+                while (string.IsNullOrWhiteSpace(credentials) || !(credentials.Split().Length > 1 && credentials.Split().Length < 3))
+                {
+                    Console.WriteLine("The field is required!");
+                    Console.WriteLine("Please enter your Email and Password seperated by a space");
+                    credentials = Console.ReadLine();
+                }
+                var email = credentials.Split()[0].Trim().ToLower();
+                var passWord = credentials.Split()[1];
+                return ValidateLogin(email, passWord);
+            }
+        }
+
+        public static bool ValidateLogin(string email, string password)
         {
             if(_user.Email.ToLower() == email.ToLower() && _user.Password == password)
             {
@@ -64,10 +73,12 @@ namespace OOP
                 _user.ToggleAgePrivacy();
 
                 Console.WriteLine($"Welcome, {_user.FullName} Your Age is {_user.Age} Joined: {_user.Created}");
+                return true;
             }
             else
             {
                 Console.WriteLine("Invalid Login Details");
+                return false;
             }
         }
 
